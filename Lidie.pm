bdd45142416ae86de786cfce3ec762bf8421352c
Stephen O. Lidie <sol0@Lehigh.EDU>
2002-10-04 16:59:04 +0000

    Import of LUSOL/Bundle-Lidie_Tk-1.0 from CPAN.
    gitpan-cpan-distribution: Bundle-Lidie_Tk
    gitpan-cpan-version:      1.0
    gitpan-cpan-path:         LUSOL/Bundle-Lidie_Tk-1.0.tar.gz
    gitpan-cpan-author:       LUSOL
    gitpan-cpan-maturity:     released

---
+++ Lidie.pm
@@ -0,0 +1,71 @@
+$Bundle::Lidie_Tk::VERSION = '1.0';
+
+package Bundle::Lidie_Tk;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Lidie_Tk - Snapshot of my Tk modules on Cube0 on Fri Oct  4 10:00:29 2002
+
+=head1 SYNOPSIS
+
+perl -MCPAN -e 'install Bundle::Lidie_Tk'
+
+=head1 CONTENTS
+
+Tk::CollapsableFrame 1.0
+
+Tk::Copy::Mac 1.3
+
+Tk::ExecuteCommand 1.2
+
+Tk::LCD 1.1
+
+Tk::LockDisplay 1.3
+
+Tk::PhotoRotate 1.0
+
+Tk::ProgressBar::Mac 1.1
+
+Tk::Splashscreen 1.0
+
+Tk::Thumbnail 1.0
+
+=head1 CONFIGURATION
+
+Summary of my perl5 (revision 5.0 version 8 subversion 0) configuration:
+  Platform:
+    osname=darwin, osvers=6.0, archname=darwin
+    uname='darwin Cube0.cc.lehigh.edu 6.0 darwin kernel version 6.0: sat jul 27 13:18:52 pdt 2002; root:xnuxnu-344.obj~1release_ppc power macintosh powerpc '
+    config_args='-des'
+    hint=recommended, useposix=true, d_sigaction=define
+    usethreads=undef use5005threads=undef useithreads=undef usemultiplicity=undef
+    useperlio=define d_sfio=undef uselargefiles=define usesocks=undef
+    use64bitint=undef use64bitall=undef uselongdouble=undef
+    usemymalloc=n, bincompat5005=undef
+  Compiler:
+    cc='cc', ccflags ='-pipe -fno-common -no-cpp-precomp -fno-strict-aliasing',
+    optimize='-O3',
+    cppflags='-no-cpp-precomp -pipe -fno-common -no-cpp-precomp -fno-strict-aliasing'
+    ccversion='', gccversion='3.1 20020420 (prerelease)', gccosandvers=''
+    intsize=4, longsize=4, ptrsize=4, doublesize=8, byteorder=4321
+    d_longlong=define, longlongsize=8, d_longdbl=define, longdblsize=8
+    ivtype='long', ivsize=4, nvtype='double', nvsize=8, Off_t='off_t', lseeksize=8
+    alignbytes=8, prototype=define
+  Linker and Libraries:
+    ld='cc', ldflags =' -flat_namespace'
+    libpth=/usr/lib
+    libs=-lm -lc
+    perllibs=-lm -lc
+    libc=/usr/lib/libc.dylib, so=dylib, useshrplib=true, libperl=libperl.dylib
+    gnulibc_version=''
+  Dynamic Linking:
+    dlsrc=dl_dyld.xs, dlext=bundle, d_dlsymun=undef, ccdlflags=' '
+    cccdlflags=' ', lddlflags=' -flat_namespace -bundle -undefined suppress'
+
+=head1 AUTHOR
+
+This Bundle has been generated automatically by the autobundle routine in CPAN.pm.
